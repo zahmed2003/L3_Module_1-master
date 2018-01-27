a2eff4b9655bff94d1634652a0d40328aab64205
zahmed2003 <league@WTS-iMac-11.attlocal.net>
2018-01-27 14:30:33 -0800

    27 January 2018 I

--- src/IntroToJUnitTest/MoreAndMoreTests.java
+++ src/IntroToJUnitTest/MoreAndMoreTests.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 
 public class MoreAndMoreTests {
 	
-	int prime = 0;
 
 	public int multiply(int a, int b)
 	{
@@ -21,25 +20,39 @@ public class MoreAndMoreTests {
 	{
 		for(int i = 2; i < a; i++)
 		{
-			if(a%i == 1)
+			if(a%i == 0)
 			{
-				prime += 1;
+				return false;
 			}
 			
 		}
-		if(prime == a - 2)
-		{
-			prime = 0;
-			return true;
-			
-		}
-		else
-		{
-			return false;
-		}
+		return true;
 		
 	}
 	
+	public boolean isSquare(int b)
+	{
+		for(int i = 0; i <= b; i++)
+		{
+			if (i*i == b)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean isCube(int c)
+	{
+		for(int i = 0; i <= c; i++)
+		{
+			if (i*i*i == c)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 	//@Test
 	//  public void MultTest() {
 
@@ -48,44 +61,44 @@ public class MoreAndMoreTests {
 	//    assertEquals(88, multiply(8, 11));
 	//  }
 
-	@Test
-	  public void PrimeTest() {
+	//@Test
+	 // public void PrimeTest() {
 
-		assertTrue(isPrime(3));
-	    assertTrue(isPrime(5));
-	    assertTrue(isPrime(541));
-	    assertFalse(isPrime(4));
-	    assertFalse(isPrime(12));
-	    assertFalse(isPrime(527));
+		//assertTrue(isPrime(3));
+	   // assertTrue(isPrime(5));
+	   // assertTrue(isPrime(541));
+	   // assertFalse(isPrime(4));
+	   // assertFalse(isPrime(12));
+	   // assertFalse(isPrime(527));
 
-	  }
+	 // }
 
-//	@Test
-//	  public void SquareTest() {
-//
-//	    assertTrue(isSquare(4));
-//	    assertTrue(isSquare(144));
-//	    assertTrue(isSquare(64));
-//	    assertTrue(isSquare(10201));
-//	    assertTrue(isSquare(1));
-//	    assertFalse(isSquare(3));
-//	    assertFalse(isSquare(22));
-//	    assertFalse(isSquare(143));
-//
-//	  }
-//
-//	@Test
-//	  public void CubeTest() {
-//
-//	    assertTrue(isCube(27));
-//	    assertTrue(isCube(216));
-//	    assertTrue(isCube(729));
-//	    assertTrue(isCube(1));
-//	    assertFalse(isCube(3));
-//	    assertFalse(isCube(22));
-//	    assertFalse(isCube(143));
-//
-//	  }
+	//@Test
+	//  public void SquareTest() {
+
+	  //  assertTrue(isSquare(4));
+	  //  assertTrue(isSquare(144));
+	  //  assertTrue(isSquare(64));
+	  //  assertTrue(isSquare(10201));
+	  //  assertTrue(isSquare(1));
+	  //  assertFalse(isSquare(3));
+	  //  assertFalse(isSquare(22));
+	  //  assertFalse(isSquare(143));
+
+	//  }
+
+	//@Test
+	 // public void CubeTest() {
+
+	   // assertTrue(isCube(27));
+	   // assertTrue(isCube(216));
+       // assertTrue(isCube(729));
+	   // assertTrue(isCube(1));
+	   // assertFalse(isCube(3));
+	  //  assertFalse(isCube(22));
+	   // assertFalse(isCube(143));
+
+	//  }
 
 
 
--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -3,9 +3,101 @@ package SearchingAndSortingAlgorithms;
 import java.util.List;
 
 public class Algorithms {
+	
+	
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0; //<- this needs changing
+		
+		for(int i = 0; i <= eggs.size(); i++)
+		{
+			if(eggs.get(i).equals("cracked"))
+			{
+				return i;
+			}
+		}
+		return 0;
+		
 	}
 	
-	//Add other methods here
+public static int countPearls(List<Boolean> oyesters) {
+	
+	 int numPearls = 0;
+	
+		for(int i = 0; i < oyesters.size(); i++)
+		{
+			if(oyesters.get(i) == true)
+			{
+				numPearls +=1;
+			}
+		}
+		
+		return numPearls;
+	}
+
+public static double findTallest(List<Double> peeps) {
+	Double tallest = 0.0;
+	
+	for(int i = 0; i < peeps.size(); i++)
+	{
+		if(peeps.get(i) > tallest)
+		{
+			tallest = peeps.get(i);
+		}
+	}
+	return tallest;
+}
+
+public static String findLongestWord(List<String> words) {
+	
+	 String longest = "";
+	 
+	 for(int i = 0; i < words.size(); i++)
+	 {
+		 if(words.get(i).length() > longest.length())
+		 {
+			 longest = words.get(i);
+		 }
+	 }
+	 
+	 
+	 return longest;
+}
+
+public static boolean containsSOS(List<String> message) {
+	 
+	 for(int i = 0; i < message.size(); i++)
+	 {
+		 if(message.get(i).contains("... --- ..."))
+		 {
+			return true;
+		 }
+	 }
+	 return false;
+}
+
+public static List<Double> sortScores(List<Double> results)
+{
+		results.sort(null);
+		
+	return results;
+}
+
+public static List<String> sortDNA(List<String> unsortedSequences) {
+	
+	unsortedSequences.sort(null);
+		
+	return unsortedSequences;
+	
+}
+
+//public static List<String> sortWords(List<String> words) {
+	// for(int i = 0; i < words.size(); i++)
+	// {
+	//	 
+	// }
+	
+//}
+
+
+
+
 }
